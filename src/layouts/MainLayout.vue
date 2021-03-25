80c86e5adca126db1f47b573f8aca2263077cec3
MyroslavShymon <myroslavshymon@gmail.com>
2021-03-25 23:44:15 +0200

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="app-main-layout">
+    <Navbar @click="isOpen = !isOpen"></Navbar>
+    <Sidebar :isOpen="isOpen"></Sidebar>
+    <main class="app-content" :class="{ full: !isOpen }">
+      <div class="app-page">
+        <router-view></router-view>
+      </div>
+    </main>
+    <FixedButton></FixedButton>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/app/Navbar";
+import Sidebar from "@/components/app/Sidebar";
+import FixedButton from "@/components/app/FixedButton";
+export default {
+  name: "main-layout",
+  data() {
+    return {
+      isOpen: true,
+    };
+  },
+  components: {
+    Navbar,
+    Sidebar,
+    FixedButton,
+  },
+};
+</script>
